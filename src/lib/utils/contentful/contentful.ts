d6536baae1c2b18f2a05fb6c12618559befd1205
code-mattclaffey <mattclaffey93@gmail.com>
2024-01-05 13:19:42 +0000

    Wire up contentful from the old site using the same pattern

---
+++ src/lib/utils/contentful/contentful.ts
@@ -0,0 +1,95 @@
+import contentful from "contentful";
+import { BLOCKS } from '@contentful/rich-text-types';
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+import generateRssFeed from "../generateRssFeed";
+
+export const client = contentful.createClient({
+  space: import.meta.env.CONTENTFUL_SPACE as string,
+  accessToken: import.meta.env.CONTENTFUL_ACCESSTOKEN as string
+});
+
+const options = {
+  renderNode: {
+    [BLOCKS.PARAGRAPH]: (field: any, next: any) => {
+      const hasCodeSnippet = field.content[0].marks.find((mark: any) => mark.type === 'code')
+
+      if (field.content.length === 1 && hasCodeSnippet) {
+        return `<pre>${next(field.content)}</pre>`;
+      }
+
+      return `<p>${next(field.content)}</p>`;
+    },
+    [BLOCKS.EMBEDDED_ASSET]: (currentField: any) => {
+      const { data: { target: { fields } } } = currentField
+
+      if (fields.file.contentType === 'video/mp4') {
+        return `<video autoplay loop muted playsinline src="${fields.file.url}">${fields.title}</video>`
+      }
+
+      return `<img src="${fields.file.url}" loading="lazy" alt="${fields.title}"/>`
+    }
+  },
+};
+
+export type BlogPost = {
+  blogTitle: string
+  blogShortDescription: string
+  blogContent: string
+  publishDate: string
+  updatedDate: string
+  blogSlug: string
+  thumbnail: string
+}
+
+export const getAllContentfulPages = async () => {
+  try {
+    const entries = await client.getEntries({ content_type: 'page', order: 'sys.createdAt' })
+
+    return entries;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getContentfulPage = async (slug: string) => {
+  const entries = await client.getEntries({ content_type: 'page', order: 'sys.createdAt' })
+
+  return entries.items.find((page: any) => {
+    return page.fields.pageSlug === slug;
+  });
+};
+
+export const getBlogEntry = async (post: any) => {
+  const entry = await client.getEntry(post.sys.id);
+
+  return {
+    blogTitle: entry.fields.blogTitle,
+    blogSlug: entry.fields.blogSlug,
+    blogShortDescription: entry.fields.blogShortDescription,
+    publishDate: entry.fields.publishDate,
+    updatedDate: entry.sys.updatedAt,
+    blogContent: documentToHtmlString(entry.fields.blogContent, options)
+  };
+}
+
+export const getContentfulBlogPosts = async () => {
+  try {
+    const entries = await client.getEntries({ content_type: 'blogPost', order: 'sys.createdAt' });
+
+    const pages = entries.items.map((page: any) => {
+      page.fields.publishDate = page.fields.publishDate;
+      page.fields.updatedDate = page.sys.updatedAt;
+      page.fields.blogContent = documentToHtmlString(page.fields.blogContent, options);
+
+      return page.fields
+    }).sort((a: any, b: any) =>  {
+      // @ts-ignore
+      return new Date(b.publishDate) - new Date(a.publishDate)
+    });
+
+    await generateRssFeed(pages);
+    return pages;
+  } catch (error) {
+    console.error(error);
+  }
+};
